f1ff0012e491dd776acec8923f20e0896cb81a7a
Pawel Kryvytskyi <pawel.kryvytskyi@traveltech.pl>
2020-04-08 18:32:31 +0200

    Updates

---
+++ precache-manifest.8d5de4f6b4791db446c43d5a4b5846e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42ea95fb12d48dd74fee2929dcf1eac1",
+    "url": "/bookSearcherReact/index.html"
+  },
+  {
+    "revision": "f12228eee667d05b2f50",
+    "url": "/bookSearcherReact/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "779a5f69cade135d88cf",
+    "url": "/bookSearcherReact/static/css/main.72abcb38.chunk.css"
+  },
+  {
+    "revision": "f12228eee667d05b2f50",
+    "url": "/bookSearcherReact/static/js/2.586c7a91.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/bookSearcherReact/static/js/2.586c7a91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "779a5f69cade135d88cf",
+    "url": "/bookSearcherReact/static/js/main.b2cad7b6.chunk.js"
+  },
+  {
+    "revision": "7419ce5911b8b83f5a57",
+    "url": "/bookSearcherReact/static/js/runtime-main.69595aaa.js"
+  }
+]);
